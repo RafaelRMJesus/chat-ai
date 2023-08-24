95886bb49822f73cda4985bbed95c27953650605
RafaelRMJesus <rafaelrocha191919@gmail.com>
2023-08-24 18:31:20 -0300

    first

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+const RootPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+
+  );
+}
+ 
+export default RootPage;
