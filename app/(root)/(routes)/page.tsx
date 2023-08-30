507f41c6a17c68e7c2107450c92ea3deb3a4139f
RafaelRMJesus <rafaelrocha191919@gmail.com>
2023-08-29 21:48:45 -0300

    persons cards

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,38 @@
+import { Persons } from "@/components/persons";
 import Categories from "@/components/ui/categories";
 import { SearchInput } from "@/components/ui/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async  () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string
+  }
+}
+
+const RootPage = async ({
+  searchParams
+}: RootPageProps) => {
+
+
+  const data = await prismadb.person.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true
+        }
+      }
+    }
+  })
 
   const categories = await prismadb.category.findMany();
 
@@ -10,6 +40,7 @@ const RootPage = async  () => {
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Persons data={data}/>
     </div>
   );
 }
